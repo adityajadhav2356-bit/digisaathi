924da63f100e53390a1d07fed6994db25c979ae9
AdityaJadhav <adityajadhav2356@gmail.com>
2026-05-22 19:55:46 +0530

    Update server to bind to 0.0.0.0 for Cloud Run

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
     res.send('DigiSaathi API is running');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
